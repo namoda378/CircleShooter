78063e90458154a2fe5deeeec954feb748b71fcd
unknown <namoda378@gmail.com>
2020-02-01 13:13:00 +0900

    --

---
+++ lua_basic.lua
@@ -0,0 +1,13 @@
+-- local, .. , +-/* 
+
+local namo1 = 5
+
+local cy = 6 
+
+local my_dogs_name = "DDuby"
+local his_dogs_name = "Mimi"
+
+print( my_dogs_name + his_dogs_name  )
+
+print (my_dogs_name .. cy .. namo)
+
