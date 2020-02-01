571cee866566900908629fa7a3318bdfea9aaac4
unknown <namoda378@gmail.com>
2020-02-01 13:46:51 +0900

    --

---
+++ lua_basic/lua_basic_3.lua
@@ -0,0 +1,18 @@
+a = a + 4
+
+a = "namo"
+
+-- print(a)
+
+function dosth(x)
+
+  local a = " nana "
+
+  print(" do " .. a .. "sth is called with " .. x)
+
+end
+
+-- for loop
+for i,v in ipairs({"apple","bee","cat"}) do 
+  dosth(v)
+end
